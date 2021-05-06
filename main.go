2c8fa56064f1ef89ab91a3b9849a59e923206d25
Behnam Ousat <behnam.ousat@gmail.com>
2021-05-05 21:51:16 -0600

    add main

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"crypto/sha256"
+	"fmt"
+)
+
+
+type Block struct {
+	Hash     []byte
+	Data     []byte
+	PrevHash []byte
+}
+type BlockChain struct{
+	blocks []*Block
+}
+
+func (b *Block) putHash() {
+	info := append(b.Data, b.PrevHash...)
+	// This will join our previous block's relevant info with the new blocks
+	hash := sha256.Sum256(info)
+	//This performs the actual hashing algorithm
+	b.Hash = hash[:] 
+	//If this ^ doesn't make sense, you can look up slice defaults
+ }
+
+
+ func CreateBlock(data string, prevHash []byte) *Block {
+	block := &Block{Hash:[]byte{}, Data:[]byte(data), PrevHash:prevHash}
+	block.putHash()
+	return block
+}
+
+
+func (chain *BlockChain) AddBlock(data string) {
+	prevBlock := chain.blocks[len(chain.blocks)-1]
+	new := CreateBlock(data, prevBlock.Hash)
+	chain.blocks = append(chain.blocks, new)
+}
+
+func Genesis() *Block {
+	return CreateBlock("Genesis", []byte{})
+}
+
+func InitBlockChain() *BlockChain {
+	return &BlockChain{ []*Block{ Genesis() }}
+}
+
+func main() {
+
+	chain := InitBlockChain()
+
+	chain.AddBlock("first")
+	chain.AddBlock("second")
+	chain.AddBlock("third")
+
+	for _, block := range chain.blocks {
+		 fmt.Printf("-------------------\n")
+		 fmt.Printf("Previous hash: %x\n", block.PrevHash)
+		 fmt.Printf("data: %s\n", block.Data)
+		 fmt.Printf("hash: %x\n", block.Hash)
+	}
+
+}
